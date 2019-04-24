aa4d3c19c34222dea7ab07c97f72caedf875de4d
William Lew <WilliamMilesLew@gmail.com>
2019-04-23 19:44:07 -0700

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var clientList = [];
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-server.listen(3000);
+server.listen(process.env.PORT);
 
 app.get('/*', function(req, res) { 
 	res.header("Access-Control-Allow-Origin", "*");
