2d4d343d0c86a4ad27b7da42287fe550478539b4
William Lew <WilliamMilesLew@gmail.com>
2019-04-24 12:20:35 -0700

    made tilemap server-side

--- src/client.js
+++ src/client.js
@@ -1,9 +1,13 @@
+var TILE_MAP;
 var clientList = [];
 
 //var socket = io.connect('http://localhost');
 var socket = io.connect('https://freetacos.herokuapp.com');
 
 socket.on('initial_connection', (data) => {
+	TILE_MAP = data.tile_map;
+	initialize();
+
 	console.log('Connected with ID: ' + socket.id);
 	socket.emit('verified_connection', { position: [joiner.player.rectangle.x, joiner.player.rectangle.y] });
 
